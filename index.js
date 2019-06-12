cda47d742a78ee54c4bd5e7e9e9dc3d8421097f3
Sachin Mahajan <sachinm78@gmail.com>
2019-06-12 16:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function lowerCaseDrivers (list) {
 
 function nameToAttributes (list) {
   return list.map(function(driver) {
+    const driverFirst = driver.split(' ')[0]
+    const driverLast = driver.split(' ')[1]
     
     return { firstName: driverFirst, lastName: driverLast }
   })
